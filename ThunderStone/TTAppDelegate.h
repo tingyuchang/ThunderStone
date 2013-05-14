6a89f4a8952c782b0a3524784a35af3881014afc
tingyugenius <tingyugenius@gmail.com>
2013-05-14 15:21:29 +0800

    Initial Commit

---
+++ ThunderStone/TTAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  TTAppDelegate.h
+//  ThunderStone
+//
+//  Created by TingYu Chang on 13/5/14.
+//  Copyright (c) 2013年 TingYu Chang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TTAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ ThunderStone/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  ThunderStone
+//
+//  Created by TingYu Chang on 13/5/14.
+//  Copyright (c) 2013年 TingYu Chang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "TTAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([TTAppDelegate class]));
+    }
+}
